2794d95f4610560af9da8a9d0fe48021a2189c1f
Luis <iluisflores@yahoo.com>
2022-03-16 00:59:28 -0500

    "Show page now functional"

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Index from "../pages/Index";
 import Show from "../pages/Show";
 
@@ -26,17 +26,46 @@ function Main(props) {
     getPeople();
   };
 
+  const updatePeople = async (person, id) => {
+    // make put request to create people
+    await fetch(URL + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    getPeople();
+  };
+
+  const deletePeople = async (id) => {
+    // make delete request to create people
+    await fetch(URL + id, {
+      method: "DELETE",
+    });
+    getPeople();
+  };
+
   useEffect(() => getPeople(), []);
 
     return (
         <main>
-          <Routes>
-            <Route
-             exact path="/" 
-             element={<Index people={people} createPeople={createPeople}/>}
-             />
-            <Route path="/people/:id" element={(rp) => <Show {...rp} />} />
-          </Routes>
+          <Switch>
+            <Route exact path="/">
+              <Index people={people} createPeople={createPeople}/>
+            </Route>
+            <Route 
+            path="/people/:id" 
+            render={(ep) => (
+              <Show 
+                {...ep} 
+                people={people}
+                updatePeople={updatePeople}
+                deletePeople={deletePeople}
+              />
+              )} 
+              />
+          </Switch>
         </main>
       );
 }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,8 +1,15 @@
-
 function Show(props) {
-    return (
-        <h1>Show Component</h1>
-      );
+  const id = props.match.params.id;
+  const people = props.people;
+  const person = people.find((p) => p._id === id);
+
+  return (
+    <div className="person">
+      <h1>{person.name}</h1>
+      <h2>{person.title}</h2>
+      <img src={person.image} alt={person.name} />
+    </div>
+  );
 }
 
 export default Show;
